2ba3d6f0df90b025e1c7177de5a21a64a4f04f94
DevFerreira669 <contatotferreira2003@gmail.com>
2026-09-17 11:01:23 -0300

    Treino1_CalculadoraSimples

---
+++ DW/Treino JS/Treino 01/main.js
@@ -0,0 +1,12 @@
+const formulario = document.getElementById("calcForm")
+
+formulario.addEventListener("submit", function(e){ e.preventDefault();
+    
+    const a = Number(document.getElementById("num1").value)
+    const b = Number(document.getElementById("num2").value)
+
+    const soma = a+b
+
+    document.getElementById("resultado").textContent = soma
+
+})
