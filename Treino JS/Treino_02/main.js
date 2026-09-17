a1544b14009d5a7b827e7ac40a5093237accac89
DevFerreira669 <contatotferreira2003@gmail.com>
2026-09-17 11:18:55 -0300

    Treino_02

---
+++ Treino JS/Treino_02/main.js
@@ -0,0 +1,32 @@
+// ========================================
+// CALCULADORA
+// ========================================
+
+// 1. Pegue o elemento "visor"
+
+
+// 2. Pegue todos os botões de números
+
+
+// 3. Faça os números aparecerem no visor
+
+
+// 4. Faça o botão C limpar o visor
+
+
+// 5. Faça o botão de apagar funcionar
+
+
+// 6. Faça os operadores funcionarem
+// +  -  *  /
+
+
+// 7. Faça o botão = calcular o resultado
+
+
+// 8. Faça o botão "." funcionar
+
+
+// DESAFIO EXTRA:
+// Faça a calculadora funcionar usando
+// o teclado do computador.
