e6c4c8b7f6b882293d655e4530857ed5416408f1
DevFerreira669 <contatotferreira2003@gmail.com>
2026-09-17 09:32:42 -0300

    AttAula_1709

--- DW/DevJS/main.js
+++ DW/DevJS/main.js
@@ -1,2 +1,19 @@
-alert ("Ola Ferreira")
-    
+/*Tipos de variáveis
+var -> escopo global
+let -> escopo local
+const -> escopo flex, valor será o mesmo até o fim do programa ex: Uma senha*/
+
+
+/*criando variaveis*/
+var valorUm = 5
+var valorDois = 11
+
+
+/*criando uma função*/
+function somar(a,b){
+    let c = (a+b)
+    return (c)
+}
+
+console.log(somar(valorUm, valorDois))
+
